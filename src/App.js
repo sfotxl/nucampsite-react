1d3b3844c9ca2bea0a748a612452b57a42170986
Kalani Man <kromelo@icloud.com>
2022-05-12 14:16:08 -0500

    Homepage Display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
-import CampsitesList from "./features/campsites/CampsitesList";
-import Footer from './components/Footer';
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,17 @@
+import {CAMPSITES} from '../../app/shared/CAMPSITES';
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(Math.random()*CAMPSITES.length)];
+// }
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+};
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+}
