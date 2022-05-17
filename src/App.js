61274aa81f73b14f848f6c95f7505ee43c98ba3c
Kalani Man <kromelo@icloud.com>
2022-05-17 13:39:20 -0500

    display card animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='contact' element={<ContactPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+          <Route path='about' element={<AboutPage/>} />
       </Routes>
       <Footer />
     </div>
