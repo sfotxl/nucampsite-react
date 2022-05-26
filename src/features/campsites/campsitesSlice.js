a6802045516337fed500bdedc3863b10850f7329
Kalani Man <kromelo@icloud.com>
2022-05-26 14:08:23 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,7 +1,19 @@
-import {CAMPSITES} from '../../app/shared/CAMPSITES';
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  campsitesArray: CAMPSITES,
+};
+
+const campsitesSlice = createSlice({
+  name: "campsites",
+  initialState,
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
-    return CAMPSITES;
+  return CAMPSITES;
 };
 
 // export const selectRandomCampsite = () => {
@@ -9,9 +21,9 @@ export const selectAllCampsites = () => {
 // }
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite) => campsite.featured);
-}
+  return CAMPSITES.find((campsite) => campsite.featured);
+};
