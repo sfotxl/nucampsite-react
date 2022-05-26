881ffd2cb0d71b53cfde7ec23378827d7b658e21
Kalani Man <kromelo@icloud.com>
2022-05-26 15:15:56 -0500

    Redux for All

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,9 +1,22 @@
 import { PARTNERS} from '../../app/shared/PARTNERS';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const selectAllPartners = () => {
-    return PARTNERS;
+const initialState = {
+    partnersArray: PARTNERS
+};
+
+const partnersSlice = createSlice({
+    name: 'partners',
+    initialState
+});
+
+export const partnersReducer = partnersSlice.reducer;
+
+export const selectAllPartners = (state) => {
+    return state.partners.partnersArray;
 }
 
-export const selectFeaturedPartner = () => {
-    return PARTNERS.find((partner) => partner.featured)
+export const selectFeaturedPartner = (state) => {
+    return state.partners.partnersArray.find((partner) => partner.featured)
 }
+
